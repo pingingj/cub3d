811a0564ace0312f10f7861077f9338089c518f5
dgarcez- <dgarcez-@c2r8s6.42lisboa.com>
2025-08-19 14:26:34 +0100

    main

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,47 @@
 /*   By: dgarcez- <dgarcez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 15:04:14 by dgarcez-          #+#    #+#             */
-/*   Updated: 2025/08/19 13:49:00 by dgarcez-         ###   ########.fr       */
+/*   Updated: 2025/08/19 14:25:19 by dgarcez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/cub3d.h"
 
-int	main()
+void	print_errors(t_game *game, int error)
 {
-	
+	if (error == 1)
+		ft_dprintf(2, "Error\nNo file provided");
+	if (error == 2)
+		ft_dprintf(2, "Error\nFile is not in the correct format");
+	if (error == 3)
+		ft_dprintf(2, "Error\nNo map provided Bruh\n");
+	if (error == 4)
+	{
+		free_map(game);
+		ft_dprintf(2, "Error\nInvalid Map\n");
+	}
+	if (error == 5)
+	{
+		free_map(game);
+		ft_dprintf(2, "Map, is too big\n");
+	}
+	exit(error);
+}
+
+// fd = open(argv[1], O_RDONLY);
+// 	if (fd == -1)
+// 		print_errors(&game, )
+
+int	main(int argc, char **argv)
+{
+	t_game	game;
+	int		fd;
+
+	if (argc != 2)
+	{
+		print_errors(&game, 1);
+		return (1);
+	}
+	parse(argv[1], fd);
+	return (0);
 }
