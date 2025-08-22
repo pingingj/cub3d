e28475043987dfd0796dab9750dc0680affc1208
Avila vm <diogo.soeiroavila@gmail.com>
2025-08-22 03:04:23 +0100

    added comments i finnaly understood all the meth, its 3:03 AM plese help me

--- srcs/generation/creation.c
+++ srcs/generation/creation.c
@@ -40,9 +40,12 @@ static void	player_init(t_game *game)
 
 
 
-/*so plane is the fov basicly, camera ex is where the camera(only x because it only moves horizontly
-) plane is the fov, i use the plane and camera to get the actual fov(the triangle), and the i use the dir(direction
-the player is looking to) to "draw" the ray to hit a wall*/
+/*->dir its the direction, like if dirx = 1 & diry = 0, we are looking rigth
+  ->plane is a perpendicular vector to the direction, it determines the FOV.
+  ->camerax indicates how much the ray is leaning to the left/rigth(valors go between [-1,1])
+  ->raydir are the vector that when added will give me the ray,so basicly camra x tells me wich side the ray is
+  the math to get raydirx/y is basicly connecting the the player the pointy part of the triangle
+  to the base,using the direction he is looking, the fov, and how far left or rigth the ray should be*/
 void setup_ray(t_game *game, int x)
 {
     game->meth.camerax = 2 * x / (double)WIDTH - 1;
@@ -51,6 +54,13 @@ void setup_ray(t_game *game, int x)
     game->meth.mapx = (int)game->player.posx;
     game->meth.mapy = (int)game->player.posy;
 }
+
+/*->so here, if raydir is 0 means we are following the x/y axes, and since we cant divide by 0 we just slap a big ahh number
+   cuz point the square of our grid is 1 by 1,and we are calculating how far we need to go to reach the other line
+   (vertical on raydirx other way around on y) we use fabs because it cant be negative since its a distance
+   ->so deltacalculates how far each square is from eachother following the ray, sidedis calculates how far the player is
+   from the closes line, since the player can be in a lot of diferent places in the same square since we use floats to walk around
+    ->step is just the directions the ray is going*/
 void prepare_dda(t_game *game)
 {
     if (game->meth.raydirx == 0)
@@ -83,6 +93,10 @@ void prepare_dda(t_game *game)
         game->meth.sidedisty = (game->meth.mapy + 1.0 - game->player.posy) * game->meth.deltadisty;
     }
 }
+/*->hit wall we calculate how far the ray goes until it hits a wall, so we need to add the distance the player need to reach the closes wall
+   then we just nned to add delta,because its the distance the ray walsk to go from one line to another lane in the same orientation(x or y)
+  ->we add on the map side, because we need to "walk" on the map to be able to identify the wall*/
+
 int	hit_wall(t_game *game)
 {
 	int	hit;
@@ -110,6 +124,7 @@ int	hit_wall(t_game *game)
 	return (side);
 }
 
+/*this is jsut becayse the DDA walks one square 2 much so we gotta go back one square to get the real distance*/
 double calc_wall_dist(t_game *game)
 {
     if (game->meth.orientation == 0)
@@ -117,6 +132,14 @@ double calc_wall_dist(t_game *game)
     else
         return game->meth.sidedisty - game->meth.deltadisty;
 }
+
+
+/*->after knowing how far the wall is, i will divide the how tall my screen is by the distance
+   if the distance is small, means im close when i divide a big number by a small number i get a big number
+   so the line heigh will be nig, i will draw more pixels, big wall
+   ->here the minus symbol menas go up, so to draw the wall i will go to the middle of the screen,
+   then go up half of the size of the wall and mark that pixel,the do the same to the lower half
+   so i know when to stop the wall*/
 void	wall_size(t_game *game, double walldist, int *sdraw, int *edraw)
 {
 	int	line_heigth;
@@ -132,7 +155,9 @@ void	wall_size(t_game *game, double walldist, int *sdraw, int *edraw)
 	if ((*edraw) < 0)
 		(*edraw) = 0;
 }
-
+/*->here now that i have the the heigth of the wall, i start drawing from above,so unitl
+   sdraw i draw the ceiling, when i reach sdraw and until i reach edraw i will draw the wall
+   then i just finish with the floor coolor*/
 void	artistic_moment(t_game *game, int x, int sdraw, int edraw)
 {
 	int	color;
